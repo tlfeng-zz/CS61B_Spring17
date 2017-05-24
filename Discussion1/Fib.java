c390fef7efc9e53be6165a9027f917c395b5c734
Tianli Feng <ftl94@live.com>
2017-05-24 14:36:54 +0800

    Fibonacci, include answer

--- Discussion1/Fib.java
+++ Discussion1/Fib.java
@@ -15,7 +15,25 @@ public class Fib {
 		}
 		return f;
 	}
+
+	public static int fib_ans(int n) { 
+		int a=0, b=1, temp;
+
+		for(int i=0; i<n; i++) {
+			temp = b;
+			b = a+b;
+			a = temp;
+		}
+		return a;
+	}
+
+/* Extra for experts: Complete fib2 in 5 lines or less. Your answer must be efficient.
+/* fib2(n, 0, 0, 1) returns the nth Fibonacci number, for n >=0. */
+	public static int fib2(int n, int k, int f0, int f1) {
+		if (n==k)
+			return f0;
+		else
+			return fib2(n, k + 1, f1, f0 + f1);
+	}
+
 }
-//Extra for experts: Complete fib2 in 5 lines or less. Your answer must be efficient.
-/** fib2(n, 0, 0, 1) returns the nth Fibonacci number, for n >=0. */
-//public static int fib2(int n, int k, int f0, int f1) {
--- Discussion1/FibTest.java
+++ Discussion1/FibTest.java
@@ -3,5 +3,7 @@ public class FibTest {
 	public static void main(String[] args) {
 		Fib f=new Fib();
 		System.out.println(f.fib(6));
+		System.out.println(f.fib_ans(6));
+		System.out.println(f.fib2(6,0,0,1));
 	}
 }
