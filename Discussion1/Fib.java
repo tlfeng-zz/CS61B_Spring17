c0b45ae37e10eaf5766cce6d894c14a351e1093b
Tianli Feng <ftl94@live.com>
2017-05-24 13:36:08 +0800

    iterative Fibonacci
    first iterative Fibonacci

---
+++ Discussion1/Fib.java
@@ -0,0 +1,21 @@
+/** fib(n) returns the nth Fibonacci number, for n >= 0.
+  * The Fibonacci sequence is 0, 1, 1, 2, 3, 5, 8, 13, 21, ... */
+public class Fib {	
+
+	public static int fib(int n) { 
+		int a=0, b=1, f=0;
+		if (n==0 || n==1) {
+			return f;
+		}
+
+		for(int i=0; i<n-1; i++) {
+			f = a+b;
+			a = b;
+			b = f;
+		}
+		return f;
+	}
+}
+//Extra for experts: Complete fib2 in 5 lines or less. Your answer must be efficient.
+/** fib2(n, 0, 0, 1) returns the nth Fibonacci number, for n >=0. */
+//public static int fib2(int n, int k, int f0, int f1) {
---
+++ Discussion1/FibTest.java
@@ -0,0 +1,7 @@
+public class FibTest {	
+
+	public static void main(String[] args) {
+		Fib f=new Fib();
+		System.out.println(f.fib(6));
+	}
+}
