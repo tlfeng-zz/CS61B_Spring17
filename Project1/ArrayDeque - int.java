d5800f78128118f5360a38cbed3b385f49632467
Tianli Feng <ftl94@live.com>
2017-06-02 17:00:01 +0800

    Second version
    Contain resizing. Removelast unfinished.

--- Project1/ArrayDeque - int.java
+++ Project1/ArrayDeque - int.java
@@ -1,6 +1,4 @@
-/** Array based list.
- *  @author Josh Hug
- */
+/** Array based list.*/
 
 //         0 1  2 3 4 5 6 7
 // items: [6 9 -1 2 0 0 0 0 ...]
@@ -14,7 +12,8 @@ public class ArrayDeque {
 
     /** Creates an empty list. */
     public ArrayDeque() {
-        items = new int[8];
+        int originalCapacity = 8;
+        items = new int[originalCapacity];
         size = 0;
         nextFirst=0; nextLast=0;
     }
@@ -35,8 +34,12 @@ public class ArrayDeque {
 
     /** Inserts X into the front of the list. */
     public void addFirst(int x) {
-        if(nextLast != 0)
+        // when first position already has item
+        if(size == 1 && nextFirst != 0)
             nextFirst = minusOne(nextFirst);
+
+        if(isFull())
+            items = resize("expand");
 
         items[nextFirst] = x;
         size ++;
@@ -45,9 +48,13 @@ public class ArrayDeque {
 
     /** Inserts X into the back of the list. */
     public void addLast(int x) {
-        if(nextFirst != 0)
+        // when first position already has item
+        if(size == 1 && nextFirst != 0)
             nextLast = plusOne(nextLast);
-        
+
+        if(isFull())
+            items = resize("expand");
+
         items[nextLast] = x;
         size ++;
         nextLast = plusOne(nextLast);
@@ -56,7 +63,7 @@ public class ArrayDeque {
     /** Gets the ith item in the list (0 is the front). */
     public int get(int index) {
         index += plusOne(nextFirst);
-        if (index>items.length)
+        if (index>=items.length)
             index -= items.length;
         return items[index];
     }
@@ -82,17 +89,56 @@ public class ArrayDeque {
     }
 
     public int removeFirst() {
-        nextFirst ++;
+        nextFirst = plusOne(nextFirst);
         size --;
+
+        // optimize memory
+        double usageFactor = 0.25;
+        if (items.length>=16 &&
+                (double)size()/items.length < usageFactor)
+            items = resize("shrink");
+
         return items[plusOne(nextFirst)];
     }
 
     /** Deletes item from back of the list and
       * returns deleted item. */
     public int removeLast() {
-        nextLast --;
+        nextLast = minusOne(nextFirst);
         size --;
         return items[minusOne(nextLast)];
+    }
+
+    public boolean isFull() {
+        if (size == items.length)
+            return true;
+        else
+            return false;
+    }
+
+    /** Create a bigger or smaller array,
+     * and copy original elements in the new array*/
+    public int[] resize(String mode) {
+        int[] newitems = new int[1];
+
+        if (mode.equals("expand")) {
+            int refactor = 2;
+            newitems = new int[size() * refactor];
+        }
+        if (mode.equals("shrink")) {
+            double usageFactor = 0.25;
+            int newSize = (int) Math.ceil(size() / usageFactor);
+            newitems = new int[newSize];
+        }
+
+        //take original array in order as a line
+        for (int i=0; i<this.size(); i++) {
+            newitems[i] = this.get(i);
+        }
+        nextFirst = newitems.length-1;
+        nextLast = size();
+
+        return newitems;
     }
 
     public static void main(String[] args) {
@@ -100,6 +146,17 @@ public class ArrayDeque {
         x.addFirst(3);
         x.addFirst(6);
         x.addLast(2);
-        x.get(2);
+        x.addLast(4);
+        x.addLast(5);
+        x.addLast(6);
+        x.addLast(7);
+        x.addLast(8);
+        x.addFirst(9);
+        x.removeFirst();x.removeFirst();x.removeFirst();
+        x.removeFirst();x.removeFirst();x.removeFirst();
+        x.removeFirst();
+        //x.get(2);
+        System.out.println(x.isFull());
+
     }
 } 
